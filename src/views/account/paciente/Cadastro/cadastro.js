9c0b48d4ea0a52025efb16c34cb295624a1642c7
Ursula Daniela Figueiredo Barbosa <ursula_daniela@hotmail.com>
2022-05-12 22:34:31 -0300

    Adição da tela de cadastro do paciente.

---
+++ src/views/account/paciente/Cadastro/cadastro.js
@@ -0,0 +1,52 @@
+const btn = document.querySelector("#btn-submit");
+
+btn.addEventListener("click", function (fullForm) {
+
+    fullForm.preventDefault();
+    alert("Preencha o formulário");
+
+    const Name = document.querySelector("#name");
+    const valueName = Name.value;
+
+    const CPF = document.querySelector("#cpf");
+    const valueCPF = CPF.value;
+
+    const RG = document.querySelector("#rg");
+    const valueRG = RG.value;
+
+    const DtNasc = document.querySelector("#dtnasc");
+    const valueDtNasc = DtNasc.value;
+
+    const CNS = document.querySelector("#cns");
+    const valueCNS = CNS.value;
+
+    const NomeMae = document.querySelector("#nome-mae");
+    const valueNomeMae = NomeMae.value;
+
+    const Endereco = document.querySelector("#endereco");
+    const valueEndereco = Endereco.value;
+
+    const CEP = document.querySelector("#cep");
+    const valueCEP = CEP.value;
+
+    const N = document.querySelector("#nº");
+    const valueN = N.value;
+
+    const Tel = document.querySelector("#tel");
+    const valueTel = Tel.value;
+
+    const Email = document.querySelector("#email");
+    const valueEmail = Email.value;
+
+    const Senha = document.querySelector("#senha");
+    const valueSenha = Senha.value;
+
+    const ConfSenha = document.querySelector("#confsenha");
+    const valueConfSenha = ConfSenha.value;
+
+    const PhotoUrl = document.querySelector("#photoUrl");
+    const valuePhotoUrl = PhotoUrl.value;
+
+    console.log(valueName, valueCPF, valueRG, valueDtNasc, valueCNS, valueNomeMae, valueEndereco, valueCEP, valueN, valueTel, valueEmail, valueSenha, valueConfSenha, valuePhotoUrl);
+
+});
