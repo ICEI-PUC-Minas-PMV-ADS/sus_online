c8d99fe79fc06ccae7b4e03a6f9cd15ffd088385
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-05-17 21:22:08 -0300

    fix: ajustando layout com bootstrap.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,12 @@
+import { UserController } from '../../controllers/UserController.js'
+
 /*
     Ocorre antes que o formulário seja exibido pela primeira vez.
 */
 $(document).ready(() => {
     componenteSidebar_Load();
     $('#sidebarCollapse').click(componenteSidebar_Click);
+    $('.redirect-custom').click(componentMenu_Click);
 });
 
 /*
@@ -22,4 +25,20 @@ function componenteSidebar_Click() {
     $('#sidebar, #content').toggleClass('active');
     $('.collapse.in').toggleClass('in');
     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+}
+
+/*
+
+*/
+function componentMenu_Click(event) {
+    event.preventDefault();
+    var controller = new UserController();
+    var idUserLogged = sessionStorage.getItem('user-logged');
+
+    var user = controller.findById(idUserLogged);
+
+    window.location.href = 
+        $(this).attr('link-redirect') !== 'home' ?
+            `./../${$(this).attr('link-redirect')}/${user.role}` :
+            `./../${$(this).attr('link-redirect')}/`;
 }
