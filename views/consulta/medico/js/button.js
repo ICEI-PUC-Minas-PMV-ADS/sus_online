ad12a29494e5d4da1c058117a835c3fe8d9efda2
Jvicentemelo <Jvicentemelo@users.noreply.github.com>
2022-06-10 02:42:28 +0000

    deploy: ca21e77ae9201ff249e6772815c2cb2b3e7aa9c9

---
+++ views/consulta/medico/js/button.js
@@ -0,0 +1,7 @@
+function showDiv() {
+    document.getElementById('videoFrame').style.display = "block";
+ }
+
+ function hideDiv() {
+    document.getElementById('videoFrame').style.display = "none";
+ }
