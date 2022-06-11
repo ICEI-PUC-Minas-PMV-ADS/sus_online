ac4696907090d4e240e547cd9710049a98466864
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-06-11 15:26:55 -0300

    fix: correcao estilo da imagem do usuario.

--- src/views/conta/js/cpf.js
+++ src/views/conta/js/cpf.js
@@ -40,39 +40,3 @@ btn.addEventListener('click', function(validaCpf) {
     }
 
 })
-
-/* Função usando laço de repetição
-
-function validaCpf() {
-
-    var cpf = document.querySelector("#cpf");
-    var somaNove = multiplicarNumeros(9, cpf, 10); //soma referente aos 9 primeiros dígitos.
-    var somaDez = multiplicarNumeros(10, cpf, 11);
-    var restoNove = verificador(somaNove); // para descobrir o dígito verificador.
-    var restoDez = verificador(somaDez);
-
-function multiplicarNumeros(quantidade, cpf, multiplicador) { // o parâmetro multiplicador é para fazer a soma de forma decrescente.
-
-    var numNove = cpf.substring(0, quantidade, soma = 0);  //variável referente aos 9 primeiros dígitos do cpf. Começa pela posição 0 e capta os 9 primeiros dígitos.
-
-    for (i = 0; i < numNove.length; i++) { // laço para multiplicar cada valor.
-        var numero = numNove.substring(i, 1);
-
-        soma += numero * multiplicador;
-        multiplicador--; // para decrementar o valor.
-    }
-
-    return soma;
-}
-
-function verificador(soma) { // após multiplicar os primeiros 9 valores, a função fará a divição e o resto será o dígito verificador.
-    
-    var resto = (soma * 10) % 11;
-    return resto.toString();
-}
-    if ((restoNove + restoDez) === cpf.substring(10, 11)) { // informa se o CPF é válido ou não.
-        console.log("CPF Válido!");
-    } else {
-        console.log("CPF Inválido!");
-    }
-}*/
