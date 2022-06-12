c3dfe70dab53cebd671b1c8faa750ce7c1c1dde3
JvicentePuc <52583833+Jvicentemelo@users.noreply.github.com>
2022-06-11 23:00:28 -0300

    Tela com bootstrap

--- src/views/consulta/medico/js/button.js
+++ src/views/consulta/medico/js/button.js
@@ -1,7 +1,10 @@
 function showDiv() {
     document.getElementById('videoFrame').style.display = "block";
+    document.getElementById('videoFrame2').style.display = "none";
  }
 
  function hideDiv() {
     document.getElementById('videoFrame').style.display = "none";
+    document.getElementById('videoFrame2').style.display = "block";
  }
+ 
