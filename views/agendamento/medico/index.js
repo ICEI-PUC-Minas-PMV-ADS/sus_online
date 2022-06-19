1202c8727b724e6b40145c0b966973d3c41eab67
Karenfranco23 <Karenfranco23@users.noreply.github.com>
2022-06-19 17:34:52 +0000

    deploy: a7b1a0c4c8b8dcc76534e4d487899f0c2a20d540

--- views/agendamento/medico/index.js
+++ views/agendamento/medico/index.js
@@ -1,11 +1,6 @@
-var lista = [
-    {nome: "Maria da Silva", horario: "07:00h", status: "no horário"},
-    {nome: "José de Oliveira", horario: "08:00h", status: "no horário"},
-    {nome: "Isaura Nunes ", horario: "09:00h", status: "no horário"},
-    {nome: "Augusto Noé ", horario: "10:00h", status: "em atraso"},
-    {nome: "Silvia Chagas ", horario: "11:00h", status: "no horário"},
-    
-];
+import { AgendamentoController } from "../../../controllers/AgendamentoController.js";
+import { UserController } from "../../../controllers/UserController.js";
+
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     exibirAtendimentos();
@@ -15,19 +10,26 @@ function exibirAtendimentos() {
     let tbody = document.getElementById("atendimentos");
     let conteudo = "";
 
+    var agendamentoController = new AgendamentoController();
+    var userController = new UserController();
+    var lista = agendamentoController.findAll();
+
+    console.log(lista);
+
+    
     lista.forEach(atendimento => {
         conteudo += `<tr>`;
         conteudo += `<td> `;
         conteudo += `<button type="button" class="btn btn-primary botao-agendar" style="font-size: 13px; background-color: #3a56d4; border-color: #3a56d4;"  onclick="agendar()">CONFIRMAR</button>`;
         conteudo += `<button type="button" class="btn btn-primary botao-agendar" style="font-size: 13px; background-color: #c42c12; border-color: #c42c12; margin-left: 6px;"  onclick="agendar()">CANCELAR</button> `;
         conteudo += `</td>`;
-        conteudo += `<td style = "font-family: 'Manrope', sans-serif; font-size: 18px;"><p>${atendimento.nome}</p></td>`;
-        conteudo += `<td style = "font-family: 'Manrope', sans-serif; font-size: 18px;"><p>${atendimento.horario}</p></td>`;
+        conteudo += `<td style = "font-family: 'Manrope', sans-serif; font-size: 18px;"><p>${userController.findById(atendimento.paciente_id).nome}</p></td>`;
+        conteudo += `<td style = "font-family: 'Manrope', sans-serif; font-size: 18px;"><p>${atendimento.hora}</p></td>`;
         conteudo += `<td style = "font-family: 'Manrope', sans-serif; font-size: 18px;"><p>${atendimento.status}</p></td>`;
         conteudo += `</tr>`;
     });
 
-    tbody.innerHTML = conteudo;
+   tbody.innerHTML = conteudo;
 }
 
 
