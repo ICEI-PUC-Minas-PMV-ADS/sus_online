2cb5672a4583b74c76a1d9240d23e93221101046
Jvicentemelo <Jvicentemelo@users.noreply.github.com>
2022-06-24 19:45:51 +0000

    deploy: f054ba7730be20a0b852da0a8f9e71bf822ac2db

---
+++ views/consulta/paciente/js/appointments.js
@@ -0,0 +1,42 @@
+import { UserController } from "/src/controllers/UserController.js";
+import { AgendamentoController } from "/src/controllers/AgendamentoController.js";
+
+
+$(document).ready(() => {
+   exibirAgendamentos_Load();
+});
+ 
+function exibirAgendamentos_Load() {
+   $(".tabela-de-agendamentos div").remove();
+   var agendamentoController = new AgendamentoController();
+   var userController = new UserController();
+
+   var listAgendamentos = agendamentoController.findByPacienteId(sessionStorage.getItem('user-logged'));
+   listAgendamentos.forEach(element => {
+       console.log(userController.findById(element.medico_id))
+       var agendamento = `<div class="col pb-2 agendamento">`;
+       agendamento += `<div class="agendamento-texto">`;
+       agendamento += `<div class="name">
+       <span>Dr. ${userController.findById(element.medico_id).nome}</span></div>`;
+       agendamento += `<div class="date">
+       <span class="material-icons">date_range</span>
+       <span>${formatarData(element.data)}</span></div> `;
+       agendamento += `<div class="hour">
+       <span class="material-icons">watch_later</span>
+       <span>${element.hora}h</span></div>`;
+       agendamento += `<input type="button" class="button" value="INICIAR CONSULTA" onclick="showDiv()"/>`;
+       agendamento += `</div>`;
+       agendamento += "<div>";
+       agendamento += "</div>";
+       agendamento += "</div>";
+       $("#agendamento").append(agendamento);
+   });
+}
+function formatarData(data) {
+   let dataSplit = data.split("-");
+   let dia = dataSplit[2];
+   let mes = dataSplit[1];
+   let ano = dataSplit[0];
+   return `${dia}/${mes}/${ano}`;
+}
+
--- views/consulta/paciente/js/button.js
+++
@@ -1,7 +0,0 @@
-function showDiv() {
-    document.getElementById('videoFrame').style.display = "block";
- }
-
- function hideDiv() {
-    document.getElementById('videoFrame').style.display = "none";
- }
